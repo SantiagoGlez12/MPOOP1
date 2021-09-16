65491cf7cc3e8709a3e67f34ca0f88fb05d31470
SantiagoGlez12 <54249136+SantiagoGlez12@users.noreply.github.com>
2021-09-16 11:13:47 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mpoo1;
+
+/**
+ *
+ * @author santiago
+ */
+public class HolaMundo { /** Creacion de la clase publica*/
+    
+    public static void main(String[] args){  /** Main para iniciar codigo*/
+        System.out.println("Hola mundo desde MPOO grupo 3"); /** Vodigo para imprimir frase en pantalla*/
+    }
+    
+}
